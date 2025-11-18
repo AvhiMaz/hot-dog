4f6623c613b8cd95e993d438fa75ccfd5f77c4c2
AvhiMaz <avhimazumder5@outlook.com>
2025-11-18 13:55:43 +0530

    feat(images): understand props and images

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,90 @@
 use dioxus::prelude::*;
 
+static CSS: Asset = asset!("/assets/main.css");
+
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    rsx!{"hello dx"}
+    rsx! {
+        document::Stylesheet { href: CSS }
+        h1 { "Hello" }
+        img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg", height: "300px"}
+        div {
+            button { "save" }
+            button { "skip" }
+        }
+    }
 }
 
+// include_str!() vs asset!()
+// let content = include_str!("file.txt");
+// // "file.txt" content is BAKED INTO the executable
+// // Size grows = larger binary
+//
+// asset!() (Dioxus):
+// let path = asset!("image.png");
+// // Returns: "/path/to/image-abc123.png"
+// // Does NOT include in executable
+// // Loaded at runtime via HTTP request
+//
+// ---
+// What "Unique Name" Means
+//
+// Instead of:
+// image.png  ← Same name every time
+//
+// The macro generates:
+// image-abc123.png  ← Unique hash added to filename
+// image-xyz789.png  ← Different user's browser gets different name
+//
+// Why This Matters
+//
+// 1. Prevents Name Collisions:
+// - If you have 2 images both named logo.png in different folders, they won't conflict
+//
+// 2. Improves Caching:
+// - When you update image.png, the new version gets a different hash name
+// - Browsers automatically use the new version instead of cached old version
+// - Old version is never downloaded again
+//
+// Example:
+// First release:  image-v1-abc.png
+// Second release: image-v2-xyz.png  ← Browser knows it's different, downloads new one
+//
+// using of struct and all..
+// first we create instance and then pass it in the component
+
+// #[derive(Props, PartialEq, Clone)]
+// struct Cat {
+//     name: String,
+// }
+
+// #[component]
+// fn App() -> Element {
+//     let cat = Cat {
+//         name: "chippu".to_string(),
+//     };
+//
+//     rsx! {
+//
+//         div {
+//             h1 { CatCard { cat } }
+//         }
+//         div {
+//             button {
+//                 onclick: move |_| info!("Clicked!"),
+//                 "Click Me"
+//             }
+//         }
+//     }
+// }
+//
+// #[component]
+// fn CatCard(cat: Cat) -> Element {
+//     rsx! {
+//         "hello {cat.name}"
+//     }
+// }
